bb942f4f7a2820c938dc126d405115919d456e2e
19yisus <jesusitomorales70@gmail.com>
2022-08-19 21:48:36 -0400

    nuevo diseño

---
+++ frontend/src/components/ChatComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { ChatIcon, UsersIcon } from "@heroicons/vue/solid"
+	defineProps<{
+		name: String,
+		message: String
+	}>()
+</script>
+
+<template>
+	<div class="p-2 h-24 max-w-sm mx-auto overflow-hidden text-ellipsis bg-white rounded-xl shadow-lg flex items-center space-x-4">
+		<div class="shrink-0">
+			<ChatIcon class="h-12 w-12 text-orange-500"/>
+		</div>
+		<div class="text-ellipsis h-full space-y-3 overflow-hidden">
+			<div class="text-xl font-medium text-black mt-2">{{name}}</div>
+			<p class="text-slate-500 text-ellipsis">{{message}}</p>
+		</div>
+	</div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import chat from '@/components/chat_minimizer.vue'
+  import { ChatIcon, UsersIcon } from "@heroicons/vue/solid"
+  import ChatComponent from "@/components/ChatComponent.vue"
 </script>
 
 <template>
-  <main>
-    <h1>Home view</h1>
+  <main class="bg-orange-500 h-screen w-screen overflow-y-scroll">
+    <div class="text-center font-bold text-white space-y-2">
+      <h2 class="mt-2">Aplicación de Mensajeria</h2>
+      <div class="flex flex-wrap items-center justify-between mx-auto w-3/4 p-2 rounded-md bg-orange-700">
+        <button class="flex items-center space-x-2"> 
+          <p>Contacts</p> <UsersIcon class="h-5 w-5"/> 
+        </button>
+        <button class="flex items-center space-x-2">
+          <p>Messages</p> <ChatIcon class="h-5 w-5"/>
+        </button>
+      </div>
+    </div>
+
+    <div class="w-full rounded-t-xl p-3 h-full overflow-y-scroll mt-3 m-auto bg-gray-100">
+      <ul class="space-y-1">
+        <li><ChatComponent name="rodrigo" message="You have a new messageeeeeeee eeeeeeee ee eeee eeeee eeeee eeeeeee eeeeee eeeeee ee!"/></li>
+        <li><ChatComponent name="Gabriel" message="tiene un mensaje pendejo" /></li>
+      </ul>
+      
+    </div>
   </main>
 </template>
